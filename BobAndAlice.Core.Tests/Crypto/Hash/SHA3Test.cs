2986f7c567d9f6650fa0b39fc3a4b537f7d2cb1f
Tito Silva <jt.mat@hotmail.com>
2022-09-17 10:49:20 -0300

    tests: minor adjustment to test

--- BobAndAlice.Core.Tests/Crypto/Hash/SHA3Test.cs
+++ BobAndAlice.Core.Tests/Crypto/Hash/SHA3Test.cs
@@ -19,8 +19,19 @@ namespace BobAndAlice.Core.Tests.Crypto.Hash
 
             var sha3 = new SHA3(SHA3.SHA3SupportedBitSizes.Bits224);
             var result = sha3.Hash(prng.Next(random.Next(256, 1024)).ToBinary());
-            
             Assert.Equal(224 >> 3, result.Length);
+
+            sha3 = new SHA3(SHA3.SHA3SupportedBitSizes.Bits256);
+            result = sha3.Hash(prng.Next(random.Next(256, 1024)).ToBinary());
+            Assert.Equal(256 >> 3, result.Length);
+
+            sha3 = new SHA3(SHA3.SHA3SupportedBitSizes.Bits384);
+            result = sha3.Hash(prng.Next(random.Next(256, 1024)).ToBinary());
+            Assert.Equal(384 >> 3, result.Length);
+
+            sha3 = new SHA3(SHA3.SHA3SupportedBitSizes.Bits512);
+            result = sha3.Hash(prng.Next(random.Next(256, 1024)).ToBinary());
+            Assert.Equal(512 >> 3, result.Length);
         }
     }
 }
