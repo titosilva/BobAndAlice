6741e16a0483070bfb1b7e303448df791a65a675
Tito Silva <jt.mat@hotmail.com>
2022-09-17 12:19:52 -0300

    feat: started AES implementation

---
+++ BobAndAlice.Core/Crypto/Symmetric/AES.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BobAndAlice.Core.Crypto.Symmetric
+{
+    public class AES
+    {
+        #region Block sizes supported
+        public enum AESSupportedBlockSizes
+        {
+            Bits128,
+            Bits192,
+            Bits256,
+        }
+
+        public int ToBitSize(AESSupportedBlockSizes blockSize)
+            => blockSize switch
+            {
+                AESSupportedBlockSizes.Bits128 => 128,
+                AESSupportedBlockSizes.Bits192 => 192,
+                AESSupportedBlockSizes.Bits256 => 256,
+                _ => throw new ArgumentException("Not supported AES block size"),
+            };
+
+        public int ToByteSize(AESSupportedBlockSizes blockSize)
+            => ToBitSize(blockSize) >> 3;
+        #endregion
+    }
+}
